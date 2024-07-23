5a10f75a35c3c9d5dd454e2590971a81c4250f89
JefersonMiranda7 <70769268+JefersonMiranda7@users.noreply.github.com>
2024-07-22 20:50:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tutorial/Variables.kt
@@ -0,0 +1,17 @@
+package com.example.tutorial
+
+fun main(){
+    /*val age:Int = 30
+    val largo:Long = 23
+    val diegoE:Float = 20.5f
+    val diegoD:Double = 34.3445
+
+    val name = "Dia"
+    val charExample:Char = 'a'
+
+    val boo:Boolean = true
+    */
+    val serer:String = "Hola diego"
+    println(25+50)
+    print(serer)
+}
