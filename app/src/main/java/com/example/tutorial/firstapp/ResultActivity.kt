b326817d9fd9a2025b34b3ecd8e01ab5e1a0bc42
JuCasas <71394429+JuCasas@users.noreply.github.com>
2024-07-22 22:24:36 -0500

    Nueva View
    Nueva view con interaccion entre pantallas

--- app/src/main/java/com/example/tutorial/firstapp/ResultActivity.kt
+++ app/src/main/java/com/example/tutorial/firstapp/ResultActivity.kt
@@ -1,34 +1,21 @@
 package com.example.tutorial.firstapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tutorial.R
 
-class ProjectOneActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        setContentView(R.layout.activity_project_one)
-        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
-        val etName = findViewById<AppCompatEditText>(R.id.etName)
-
-        btnStart.setOnClickListener {
-            val name = etName.text.toString()
-
-            if (name.isNotEmpty()){
-                val intent = Intent(this, ResultActivity::class.java)
-                intent.putExtra("EXTRA_NAME", name)
-                startActivity(intent)
-            }
-        }
-
+        setContentView(R.layout.activity_result)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val name:String = intent.extras?.getString("EXTRA_NAME").orEmpty()
+        tvResult.text = "Hola ${name}"
         /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -36,6 +23,5 @@ class ProjectOneActivity : AppCompatActivity() {
             insets
         }
          */
-
     }
 }
